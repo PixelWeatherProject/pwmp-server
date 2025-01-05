ce56189e811d8b1319b4b329aa8f91afbf120e1e
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-01-05 10:07:47 +0100

    filter out updates that have been already applied

--- queries/get_os_update.sql
+++ queries/get_os_update.sql
@@ -1,24 +1,35 @@
 SELECT
-  version_major,
-  version_middle,
-  version_minor,
-  firmware
+  f.version_major,
+  f.version_middle,
+  f.version_minor,
+  f.firmware
 FROM
-  firmwares
+  firmwares f
 WHERE
   (
-    restrict_nodes IS NULL
-    OR $1 = ANY (restrict_nodes)
+    f.restrict_nodes IS NULL
+    OR $1 = ANY (f.restrict_nodes)
   )
   AND (
-(version_major > $2)
+    (f.version_major > $2)
     OR (
-      version_major = $2
-      AND version_middle > $3
+      f.version_major = $2
+      AND f.version_middle > $3
     )
     OR (
-      version_major = $2
-      AND version_middle = $3
-      AND version_minor > $4
+      f.version_major = $2
+      AND f.version_middle = $3
+      AND f.version_minor > $4
     )
+  )
+  AND NOT EXISTS (
+    SELECT
+      1
+    FROM
+      firmware_stats fs
+    WHERE
+      fs.node = $1
+      AND fs.to_version_major = f.version_major
+      AND fs.to_version_middle = f.version_middle
+      AND fs.to_version_minor = f.version_minor
   );
