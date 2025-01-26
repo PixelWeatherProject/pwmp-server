1948e0a0406d84cd9f0d400d0cefc53d509a636c
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-01-26 10:12:24 +0100

    remove `UNIQUE` constraints

--- migrations/20241217193123_create_notifications_table.sql
+++ migrations/20241217193123_create_notifications_table.sql
@@ -2,8 +2,8 @@
 CREATE TABLE
     notifications (
         id SMALLSERIAL PRIMARY KEY,
-        node INT2 UNIQUE NOT NULL REFERENCES devices (id),
-        "when" TIMESTAMP UNIQUE NOT NULL DEFAULT NOW (),
+        node INT2 NOT NULL REFERENCES devices (id),
+        "when" TIMESTAMP NOT NULL DEFAULT NOW (),
         content VARCHAR(64) NOT NULL,
         read BOOLEAN NOT NULL DEFAULT FALSE
     );
