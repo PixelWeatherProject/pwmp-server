400396f29e0f8aa20622c491710f0d37cd1f968f
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-03-31 10:52:05 +0200

    refer to message length type using alias

--- src/server/client.rs
+++ src/server/client.rs
@@ -113,7 +113,8 @@ impl<S> Client<S> {
 
         // Parse the length
         let message_length: usize =
-            u32::from_be_bytes(*array_ref![self.buf, 0, size_of::<u32>()]).try_into()?;
+            MsgLength::from_be_bytes(*array_ref![self.buf, 0, size_of::<MsgLength>()])
+                .try_into()?;
 
         // Verify the length
         if message_length == 0 {
