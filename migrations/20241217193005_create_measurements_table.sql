d92fe12871602e647e0ad57afe7b3f98a0545504
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-05-11 15:41:35 +0200

    make column keep time zone info

--- migrations/20241217193005_create_measurements_table.sql
+++ migrations/20241217193005_create_measurements_table.sql
@@ -3,14 +3,16 @@ CREATE TABLE
     measurements (
         id SERIAL PRIMARY KEY,
         node INT4 NOT NULL REFERENCES devices (id),
-        "when" TIMESTAMP NOT NULL DEFAULT NOW (),
-        temperature REAL NOT NULL CHECK (
-            temperature > -100.00
-            AND temperature < 100.00
-        ),
-        humidity SMALLINT NOT NULL CHECK (
-            humidity >= 0
-            AND humidity <= 100
-        ),
-        air_pressure SMALLINT DEFAULT NULL
+        "when" TIMESTAMP
+        WITH
+            TIME ZONE NOT NULL DEFAULT NOW (),
+            temperature REAL NOT NULL CHECK (
+                temperature > -100.00
+                AND temperature < 100.00
+            ),
+            humidity SMALLINT NOT NULL CHECK (
+                humidity >= 0
+                AND humidity <= 100
+            ),
+            air_pressure SMALLINT DEFAULT NULL
     );
