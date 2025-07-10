7b005718527829e41c72df35022b5f4898399805
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-07-10 10:19:18 +0200

    add basic OpenRC detection

--- src/svcmgr/openrc.rs
+++ src/svcmgr/openrc.rs
@@ -1,11 +1,27 @@
 use super::traits::ServiceManager;
 use crate::error::Error;
+use regex::Regex;
+use std::process::Command;
+use tracing::{error, info};
 
 pub struct Manager;
 
 impl ServiceManager for Manager {
     fn detect(&self) -> bool {
-        false
+        let Ok(output) = super::exec_command(Command::new("openrc").arg("--version")) else {
+            error!("Failed to execute OpenRC CLI");
+            return false;
+        };
+
+        let version_regex = Regex::new(r#"\d+.\d+.\d+"#).unwrap();
+        let Some(version_string) = version_regex.captures(&output).and_then(|res| res.get(0))
+        else {
+            error!("Failed to parse SystemD version string");
+            return false;
+        };
+
+        info!("Found OpenRC v{}", version_string.as_str());
+        true
     }
 
     fn installed(&self) -> bool {
