673a042f74223b3d5e49439171cdab4b4bf58eab
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2026-05-27 19:22:14 +0200

    add missing `cpu_temp` to columns

--- queries/postgres/post_measurements.sql
+++ queries/postgres/post_measurements.sql
@@ -3,10 +3,10 @@ INSERT INTO measurements(
         "temperature",
         "humidity",
         "air_pressure",
+        "cpu_temp",
         "measurement",
         "battery",
         "wifi_ssid",
         "wifi_rssi"
     )
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-RETURNING id;
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
--- queries/sqlite/post_measurements.sql
+++ queries/sqlite/post_measurements.sql
@@ -1,4 +1,4 @@
 INSERT INTO
-    measurements ("node", "temperature", "humidity", "air_pressure", "measurement", "battery", "wifi_ssid", "wifi_rssi")
+    measurements ("node", "temperature", "humidity", "air_pressure", "cpu_temp", "battery", "wifi_ssid", "wifi_rssi")
 VALUES
-    (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8) RETURNING id;
+    (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9);
