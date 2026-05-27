369248f2fd11d79c5fd5a06891ec30e115eb26a6
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2026-05-27 20:00:32 +0200

    remove unused column from measurement insertion script

--- queries/postgres/post_measurements.sql
+++ queries/postgres/post_measurements.sql
@@ -4,9 +4,8 @@ INSERT INTO measurements(
         "humidity",
         "air_pressure",
         "cpu_temp",
-        "measurement",
         "battery",
         "wifi_ssid",
         "wifi_rssi"
     )
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
--- queries/sqlite/post_measurements.sql
+++ queries/sqlite/post_measurements.sql
@@ -1,4 +1,4 @@
 INSERT INTO
     measurements ("node", "temperature", "humidity", "air_pressure", "cpu_temp", "battery", "wifi_ssid", "wifi_rssi")
 VALUES
-    (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9);
+    (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8);
